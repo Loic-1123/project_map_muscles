92ee2040107f153dab6af101869cce3c6a33ac92
Loic-1123 <loic.deslarzes@epfl.ch>
2024-05-13 11:02:32 +0200

    check_labelling.py cleaned

--- map_muscles/extract_fluorescence/check_labelling.py
+++ map_muscles/extract_fluorescence/check_labelling.py
@@ -1,110 +1,109 @@
-from _root_path import add_root, get_root_path
+from _root_path import add_root
 add_root()
 
-from pathlib import Path
-
 import h5py
-import numpy as np
-import cv2
 import tqdm
 import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import numpy as np
 
 import map_muscles.path_utils as pu
 import map_muscles.video_converter as vc
 import map_muscles.extract_fluorescence.imaging_utils as imu
-import map_muscles.video_converter as vc
+import map_muscles.path_utils as pu
 
-filename = 'label_femur_V1.000_900_1440_kin.analysis.h5'
+def print_sleap_labelling_info(sleap_file_path):
+    with h5py.File(sleap_file_path, 'r') as f:
+        dset_names = list(f.keys())
+        locations = f["tracks"][:].T
+        node_names = [n.decode() for n in f["node_names"][:]]
 
-sleap_folder = pu.get_sleap_dir()
+    print("===HDF5 datasets===")
+    print(dset_names)
+    print()
 
-file_path = sleap_folder / filename
+    print("===locations data shape===")
+    print(locations.shape)
+    print("# frames, # nodes, # dimensions (x, y), # instances")
+    print()
 
-assert file_path.exists(), f"File not found: {file_path}"
+    print("===nodes: (id, name)===")
+    for i, name in enumerate(node_names):
+        print(f"{i}: {name}")
+    print()
 
-with h5py.File(file_path, 'r') as f:
-    dset_names = list(f.keys())
-    locations = f["tracks"][:].T
-    node_names = [n.decode() for n in f["node_names"][:]]
+def write_kin_labeled_video(
+        video_writer, figsize, 
+        frames, frame_start, frame_end, 
+        locations, labels,
+        instance_id=0,
+        n=1,
+        s=5, colors=[np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1])],
+        ):
 
-"""
-print("===filename===")
-print(filename)
-print()
+    print("===Creating video===")
+    r = range(frame_start, frame_end, n)
+    for i in tqdm.tqdm(r):
+        frame = frames[i]
 
-print("===HDF5 datasets===")
-print(dset_names)
-print()
+        fig, ax = plt.subplots(1,1, figsize=figsize)
+        ax.imshow(frame, cmap='gray')
 
-print("===locations data shape===")
-print(locations.shape)
-print()
+        for j, label in enumerate(labels):
+            ax.scatter(
+                locations[i, j, 0, instance_id], locations[i, j, 1, instance_id],
+                color=colors[j], s=s, label=label
+                )
 
-print("===nodes===")
-for i, name in enumerate(node_names):
-    print(f"{i}: {name}")
-print()
-"""
+        ax.set_title(f'frame {i}')
+        ax.legend()
 
-TROCHANTER_ID = 0
-FEMUR_TIBIA_ID = 1
-TIBIA_TARSUS_ID = 2
+        # remove axis
+        ax.axis('off')
 
-trochanter_locations = locations[:, TROCHANTER_ID, :, :]
-femur_tibia_locations = locations[:, FEMUR_TIBIA_ID, :, :]
-tibia_tarsus_locations = locations[:, TIBIA_TARSUS_ID, :, :]
+        vc.save_frame_plt_film(video_writer, fig)
 
-mpl.rcParams['figure.figsize'] = [15,6]
+        # close figure
+        plt.close(fig)
 
-kin_path = pu.get_kin_dir()
+    vc.end_cv2_writing(out)
+    print("Video created at ", out_path)
+    print("===END: Creating video===")
 
-kin_frames = vc.extract_kin_frames(kin_path, 'jpg')
+if __name__ == "__main__":
+    
+    filename = 'label_femur_V1.000_900_1440_kin.analysis.h5'
+    sleap_folder = pu.get_sleap_dir()
+    file_path = sleap_folder / filename
 
-kin_min_id = imu.get_min_id(kin_path, 'jpg')
+    print_sleap_labelling_info(file_path)
 
-len_max = min(len(kin_frames), len(trochanter_locations))
+    with h5py.File(file_path, 'r') as f:
+        locations = f["tracks"][:].T
 
-out_path = vc.get_video_dir()
-assert out_path.exists(), f"Following out_path does not exist: {out_path}"
+    mpl.rcParams['figure.figsize'] = [15,6]
 
-figsize = (10, 5)
+    kin_path = pu.get_kin_dir()
 
-#video_name = 'check_labelling_on_kinematic_fps6.mp4'
-video_name = 'test2.mp4'
-fps = 12
-out = vc.get_video_writer(video_name, figsize, fps=fps)
+    kin_frames = vc.extract_kin_frames(kin_path, 'jpg')
 
-frame_start = 710
-frame_end = len_max
-n = 1
+    kin_min_id = imu.get_min_id(kin_path, 'jpg')
+    len_max = min(len(kin_frames), len(locations))
 
-r = range(frame_start, frame_end, n)
-print("===Creating video===")
+    figsize = (10, 5)
+    out_path = pu.get_video_dir()
+    video_name = 'check_labelling_on_kinematic_fps12.mp4'
+    fps = 12
+    out = vc.get_video_writer(video_name, figsize, fps=fps)
 
-size = 5
+    frame_start = 710
+    frame_end = len_max 
 
-for i in tqdm.tqdm(r):
-    frame = kin_frames[i]
-
-    fig, ax = plt.subplots(1,1, figsize=figsize)
-    ax.imshow(frame, cmap='gray')
-    ax.scatter(trochanter_locations[i, 0, 0], trochanter_locations[i, 1, 0], c='r', s=size, label='trochanter')
-    ax.scatter(femur_tibia_locations[i, 0, 0], femur_tibia_locations[i, 1, 0], c='g', s=size, label='femur-tibia')
-    ax.scatter(tibia_tarsus_locations[i, 0, 0], tibia_tarsus_locations[i, 1, 0], c='b', s=size, label='tibia-tarsus')
-    ax.set_title(f'frame {i}')
-    ax.legend()
-
-    # remove axis
-    ax.axis('off')
-
-    vc.save_frame_plt_film(out, fig)
-
-    # close figure
-    plt.close(fig)
-
-vc.end_cv2_writing(out)
-
-print('Video created to ', out_path)
+    labels = ['trochanter', 'femur-tibia', 'tibia-tarsus']
+    write_kin_labeled_video(
+        out, figsize, 
+        kin_frames, frame_start, frame_end,
+        locations, labels, n=1)
+    
+    
