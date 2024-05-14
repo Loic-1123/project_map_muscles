cf4468f3665625db2f4c7c19be34d7a02ebda727
Loic-1123 <loic.deslarzes@epfl.ch>
2024-05-14 16:46:31 +0200

    imaging_utils.py clean

--- map_muscles/extract_fluorescence/imaging_utils.py
+++ map_muscles/extract_fluorescence/imaging_utils.py
@@ -1,14 +1,18 @@
 from _root_path import add_root, get_root_path
 add_root()
 
-import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
 import cv2
-from scipy.signal import find_peaks
-
 import json
 
+
+"""
+This file contains functions to handle and get muscle and kin images, 
+such has getting corresponding indices or images for muscle and kin images.
+It also contains functions to map points from kin to muscle images and vice versa,
+as well as to crop images based on points.
+"""
 
 root_path = Path(get_root_path())
 calibration_file = root_path / 'map_muscles' / 'calibration' / 'calibration_parameters.json'
@@ -26,15 +30,6 @@ ref_muscle_pts = np.array(calib['ref_muscle_pts'])
 img_path = root_path / 'map_muscles' / 'data' / '20240213_muscle_recording'
 
 assert img_path.exists(), f"Following path for recorded images does not exist: {img_path}"
-
-def get_biggest_files(files_path, n_files=100, extension='tif'):
-    """
-    Get the n_files biggest files in a directory
-    """
-
-    files = sorted(files_path.glob(f"*.{extension}"), key=lambda x: x.stat().st_size)[-n_files:]
-    return files
-
 def get_min_id(files_path, extension, id_format=None, return_str=False):
     files = sorted(files_path.glob(f"*.{extension}"))
     ids = list(map(lambda x: int(x.stem), files))
@@ -105,13 +100,10 @@ def get_matching_kin_img(
         kin_to_muscle_div_factor, 
         min_id_kin_file
         )
-    
-    kin_frame_channel = get_matching_kin_frame_channel(kin_frame_id, min_id_kin_file)
 
     kin_image_id = get_matching_kin_image_id(
         min_id_kin_file,
         kin_frame_id, 
-        kin_frame_channel
         )
     
     if format is not None:
@@ -161,136 +153,6 @@ def get_matching_muscle_img(
 
     return muscle_img
 
-
-        
-
-
-
-### ###
-def get_index_from_frame_id(frame_id, min_frame_id, min_img_id):
-    ...
-
-    #TODO
-    
-
-
-def cropped_image(muscle_image, margin=100, width=10, prominence=0.9, plot=False):
-    mean_x = np.mean(muscle_image, axis=0)
-    mean_y = np.mean(muscle_image, axis=1)
-
-    x_peaks, x_peaks_descr = find_peaks(mean_x, width=width, prominence=prominence)
-    y_peaks, y_peaks_descr = find_peaks(mean_y, width=width, prominence=prominence)
-
-    x_width = x_peaks_descr["widths"]
-    y_width = y_peaks_descr["widths"]
-
-    if plot:
-        plt.figure(figsize=(10, 5))
-        plt.plot(mean_x, label="mean x")
-        plt.plot(mean_y, label="mean y")
-        plt.scatter(x_peaks, mean_x[x_peaks], c="r", label="x peaks")
-        plt.scatter(y_peaks, mean_y[y_peaks], c="g", label="y peaks")
-        plt.legend()
-    
-    max_y_peak = np.argmax(y_peaks_descr["prominences"])
-    central_x_peak = np.argmin(np.abs(x_peaks- mean_x.shape[0]/2))
-
-    x_start = int(x_peaks[central_x_peak] - x_width[central_x_peak]) - margin
-    x_end = int(x_peaks[central_x_peak] + x_width[central_x_peak]) + margin
-
-    y_start = int(y_peaks[max_y_peak] - y_width[max_y_peak]) - margin
-    y_end = int(y_peaks[max_y_peak] + y_width[max_y_peak]) + margin
-
-    cropped_img = muscle_image[y_start:y_end, x_start:x_end]
-
-    return cropped_img
-
-
-def cropped_images(
-        muscle_image, 
-        nb_peaks:int=2, 
-        margin:int=100, 
-        width:int=10, 
-        prominence:float=0.9, 
-        sort:bool=True,
-        plot:bool=False
-        ):
-    
-    """
-    Returns the cropped images of the muscle image based on the peaks of the mean x and y
-    axis of the muscle image.
-
-    Args:
-        muscle_image: The muscle image to be cropped.
-        nb_peaks: The number of peaks to consider.
-        margin: The margin to be added to the cropped images.
-        width: The width of the peaks to be considered.
-        prominence: The prominence of the peaks to be considered.
-        sort: If True, sort the cropped images by x position.
-        plot: If True, plot information about the peaks and the selected areas.
-
-    Returns:
-        List of cropped images.
-    """
-
-    mean_x = np.mean(muscle_image, axis=0)
-    mean_y = np.mean(muscle_image, axis=1)
-
-    x_peaks, x_peaks_descr = find_peaks(mean_x, width=width, prominence=prominence)
-    y_peaks, y_peaks_descr = find_peaks(mean_y, width=width, prominence=prominence)
-
-    x_width = x_peaks_descr["widths"]
-    y_width = y_peaks_descr["widths"]
-
-    max_y_peak = np.argmax(y_peaks_descr["prominences"])
-    sorted_x_peaks = np.argsort(x_peaks_descr["prominences"])
-    max_x_peaks = sorted_x_peaks[-nb_peaks:]
-
-    x_starts = []
-    x_ends = []
-    cropped_imgs = []
-
-    y_start = int(y_peaks[max_y_peak] - y_width[max_y_peak]) - margin
-    y_end = int(y_peaks[max_y_peak] + y_width[max_y_peak]) + margin
-
-    for max_x_peak in max_x_peaks:
-        x_starts.append(int(x_peaks[max_x_peak] - x_width[max_x_peak]) - margin)
-        x_ends.append(int(x_peaks[max_x_peak] + x_width[max_x_peak]) + margin)
-
-    # sort by x position
-    if sort: x_starts, x_ends = zip(*sorted(zip(x_starts, x_ends)))
-
-    for x_start, x_end in zip(x_starts, x_ends):
-        cropped_imgs.append(muscle_image[y_start:y_end, x_start:x_end])
-
-    if plot:
-        plt.figure(figsize=(10, 5))
-        plt.plot(mean_x, label="mean x")
-        plt.plot(mean_y, label="mean y")
-        plt.scatter(x_peaks, mean_x[x_peaks], c="r", label="x peaks")
-        plt.scatter(y_peaks, mean_y[y_peaks], c="g", label="y peaks")
-        
-        # chosen peaks x and y
-        size=100
-        plt.scatter(x_peaks[max_x_peaks], mean_x[x_peaks[max_x_peaks]], 
-                    c="k", label="chosen peaks", marker="x", s=size)
-        plt.scatter(y_peaks[max_y_peak], mean_y[y_peaks[max_y_peak]], 
-                    c="k", marker="x", s=size) 
-
-        # plot segments at the bottom of the plot to show selected area
-        bottom = min(min(mean_y), min(mean_x))
-        plt.plot([x_starts[0], x_ends[0]], [bottom, bottom], c="b", linestyle="-", label="x selected ranges")
-        for x_start, x_end in zip(x_starts, x_ends):
-            plt.plot([x_start, x_end], [bottom, bottom], c="b", linestyle="-")
-            bottom = bottom - 3
-                
-        plt.plot([y_start, y_end], [bottom, bottom], linestyle="-", label="y selected range")
-
-        plt.legend()
-    
-    return cropped_imgs
-
-
 def get_rectangle(pts, half_width, margin):
     pts_vector = pts[1] - pts[0]
     pts_vector = pts_vector/np.linalg.norm(pts_vector)
@@ -331,29 +193,6 @@ def get_points_coor_for_cropped_img(pts, half_width, margin):
     point2 = half_width, margin + length
 
     return point1, point2
-
-    
-
-def draw_corresp_points(kin_pts, kin_img, muscle_img, draw_lines=True):
-    
-    muscle_pts, muscle_line_pts = map_points(kin_pts, muscle_img.shape[:2],
-                                             ret_line_pts=True)
-    
-    kin_img = cv2.cvtColor(kin_img, cv2.COLOR_GRAY2BGR).copy()
-    muscle_img = cv2.cvtColor(muscle_img, cv2.COLOR_GRAY2BGR).copy()
-
-    for kin_pt, muscle_pt, line_pt in zip(kin_pts, muscle_pts, muscle_line_pts):
-        color = tuple(np.random.randint(0, 255, 3).tolist())
-        kin_img = cv2.circle(kin_img, tuple(kin_pt), 10, color, -1)
-        muscle_img = cv2.circle(muscle_img, tuple(muscle_pt), 10, color, 2)
-        
-        if draw_lines:
-            muscle_img = cv2.line(muscle_img,
-                                  tuple(line_pt[0]),
-                                  tuple(line_pt[1]),
-                                  color, 2)
-    return kin_img, muscle_img
-
 
 def map_points(kin_pts, muscle_img_shape, ret_line_pts=False):
     ''' img_line - image on which we draw the epilines for the points in img_pts
